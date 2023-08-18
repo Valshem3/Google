68d459d375d0d9ebff1084bffcf4aeb61aea75eb
Valshem3 <valshem2@gmail.com>
2023-08-18 00:38:58 +0000

    this is my 3rd committ

---
+++ task.sh
@@ -0,0 +1,3 @@
+Source Code Management (SCM):
+Source Code Management, also known as Version Control, is a system that tracks changes to code over time. It provides a structured way to manage codebase versions, facilitating collaboration among developers, minimizing conflicts, and ensuring the integrity of the codebase. SCM allows developers
+
